907bed5c711ba533edeeec00def06099fbf9406e
jof <jof@thejof.com>
2026-06-27 23:51:50 -0700

    Rename example icon dump path some-ting -> reasoning-ting
    Last dangling some-ting reference after the project rename.

--- listener/examples/dump_icons.rs
+++ listener/examples/dump_icons.rs
@@ -1,7 +1,7 @@
 //! Dump the tray icon states as raw RGBA for preview:
-//!   cargo run --example dump_icons -- /tmp/someting-icons
+//!   cargo run --example dump_icons -- /tmp/reasoning-ting-icons
 fn main() {
-    let dir = std::env::args().nth(1).unwrap_or_else(|| "/tmp/someting-icons".into());
+    let dir = std::env::args().nth(1).unwrap_or_else(|| "/tmp/reasoning-ting-icons".into());
     std::fs::create_dir_all(&dir).unwrap();
     reasoning_ting::icon::dump_rgba(&dir).unwrap();
     println!("wrote {}x{} icons to {dir}", reasoning_ting::icon::SIZE, reasoning_ting::icon::SIZE);
